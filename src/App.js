a8a094b6aae71d04cb8f22fb2497c9914acd1a8c
Junhyoung <junh3951@me.com>
2024-08-18 23:40:14 +0900

    FETCH: build error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Search from "./screen/Search/Search";
-import Loading from "./screen/Loading/Loading";
-import { Result } from "./screen/Result/Result";
-import { ResponseProvider } from "./context/response";
-import NotFoundPage from "./screen/NotFound/NotFound";
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Search from './screen/Search/Search'
+import Loading from './screen/Loading/Loading'
+import { Result } from './screen/Result/Result'
+import NotFoundPage from './screen/NotFound/NotFound'
 
 function App() {
-  return (
-    <ResponseProvider>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Search />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/loading" element={<Loading />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="*" element={<NotFoundPage />}/>
-          </Routes>
-        </div>
-      </Router>
-    </ResponseProvider>
-  );
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Search />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/loading" element={<Loading />} />
+                    <Route path="/result" element={<Result />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
--- src/screen/Search/Search.jsx
+++ src/screen/Search/Search.jsx
@@ -1,71 +1,76 @@
 // src/screen/Search/Search.jsx
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { Header } from "../../component/header/header";
-import SearchBar from "../../component/searchbar/searchbar";
-import styles from "./Search.module.css";
-import { ResponseContext } from "../../context/response";
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Header } from '../../component/header/header'
+import SearchBar from '../../component/searchbar/searchbar'
+import styles from './Search.module.css'
 
 function Search() {
-  const navigate = useNavigate();
-  // const { setResponse } = useContext(ResponseContext);
+    const navigate = useNavigate()
+    // const { setResponse } = useContext(ResponseContext);
 
-  const handleSearch = async (searchQuery) => {
-    try {
-      navigate("/loading", { state: { searchQuery } });
-      const response = await fetch("https://trizolve.com/ideas", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          problemDescription:
-            "낚시대 / 가격은 유지하면서 고급화시키고 싶어. 방법이 없을까??",
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
-      const data = await response.json();
-      sessionStorage.setItem("idea", JSON.stringify(data));
-      // setResponse(data);
+    const handleSearch = async (searchQuery) => {
+        try {
+            navigate('/loading', { state: { searchQuery } })
+            const response = await fetch('https://trizolve.com/ideas', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    problemDescription:
+                        '낚시대 / 가격은 유지하면서 고급화시키고 싶어. 방법이 없을까??',
+                }),
+            })
+            if (!response.ok) {
+                throw new Error('Failed to fetch data')
+            }
+            const data = await response.json()
+            sessionStorage.setItem('idea', JSON.stringify(data))
+            // setResponse(data);
 
-      navigate("/result", { state: { data } });
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
+            navigate('/result', { state: { data } })
+        } catch (error) {
+            console.error('Failed to fetch data:', error)
+        }
     }
-  };
 
-  return (
-    <div className={styles.search}>
-      <Header />
-      <div className={styles.content}>
-        <img className={styles.contentLogo} alt="Logo" src="/image/logo.png" />
-        <div className={styles.contentText}>
-          <SearchBar onSearch={handleSearch} />
-          <div className={styles.infoSection}>
-            <p className={styles.textWrapper}>예) 2차전지 / 배터리 수명 연장</p>
-            <div className={styles.description}>
-              <div className={styles.alignmentContainer}>
+    return (
+        <div className={styles.search}>
+            <Header />
+            <div className={styles.content}>
                 <img
-                  className={styles.line}
-                  alt="Line1"
-                  src="/image/line1.svg"
+                    className={styles.contentLogo}
+                    alt="Logo"
+                    src="/image/logo.png"
                 />
-                <span className={styles.div}>분야</span>
-              </div>
-              <div className={styles.alignmentContainer}>
-                <img
-                  className={styles.line}
-                  alt="Line2"
-                  src="/image/line2.svg"
-                />
-                <span className={styles.div2}>문제 상황</span>
-              </div>
+                <div className={styles.contentText}>
+                    <SearchBar onSearch={handleSearch} />
+                    <div className={styles.infoSection}>
+                        <p className={styles.textWrapper}>
+                            예) 2차전지 / 배터리 수명 연장
+                        </p>
+                        <div className={styles.description}>
+                            <div className={styles.alignmentContainer}>
+                                <img
+                                    className={styles.line}
+                                    alt="Line1"
+                                    src="/image/line1.svg"
+                                />
+                                <span className={styles.div}>분야</span>
+                            </div>
+                            <div className={styles.alignmentContainer}>
+                                <img
+                                    className={styles.line}
+                                    alt="Line2"
+                                    src="/image/line2.svg"
+                                />
+                                <span className={styles.div2}>문제 상황</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Search;
+export default Search
